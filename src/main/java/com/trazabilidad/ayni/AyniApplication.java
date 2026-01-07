8c8da6224f425f296ec0262749966d32ca15dc01
Jhoneiro <jhoneiro12@hotmail.com>
2026-01-07 13:36:29 -0500

    iniciando proyecto ya actualizando librerias

---
+++ src/main/java/com/trazabilidad/ayni/AyniApplication.java
@@ -0,0 +1,13 @@
+package com.trazabilidad.ayni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AyniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AyniApplication.class, args);
+	}
+
+}
