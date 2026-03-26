afb0520552ca250f83c0457af51840ae1919b46e
Jhoneiro <jhoneiro12@hotmail.com>
2026-03-25 19:30:54 -0500

    fix: tolera campos legacy al crear actividades
    Se configura ActividadProyectoRequest para ignorar propiedades JSON desconocidas y evitar 500 cuando el cliente aun envia campos obsoletos como posicionX/posicionY durante despliegues parciales.

--- src/main/java/com/trazabilidad/ayni/proyecto/dto/ActividadProyectoRequest.java
+++ src/main/java/com/trazabilidad/ayni/proyecto/dto/ActividadProyectoRequest.java
@@ -1,5 +1,6 @@
 package com.trazabilidad.ayni.proyecto.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,6 +14,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ActividadProyectoRequest {
     private Long id;
 
